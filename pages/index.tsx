09af0aa14a0334a12645bc30abb8c520b59cc0a6
Airrocket <airrocket.buksas@gmail.com>
2021-06-27 20:25:08 -0600

    update index links

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,22 +21,24 @@ export default function Home() {
 					<h1 className="hidden md:block">@Kayt_was_taken everwhere</h1>
 					<h1 className="block md:hidden">@Kayt_was_taken</h1>
 					<div className="flex justify-center mt-8"> {/* Images Links */}
-						{/* Twitter */}
-						<div className="w-16 md:w-20 mx-8"> 
-							<a href="https://www.twitter.com/kayt_was_taken"><Image src="/icos/TwitterLogoBlack.png" alt="Twitter logo" width="70" height="57"/></a>
+						{/* Tumblr */}
+						<div className="index-image">
+							<a href="https://kayt-was-taken.tumblr.com/"><Image src="/icos/tumblr-3-64.png" alt="Tumblr logo" width="64" height="64"/></a>
 						</div>
-						{/* Twitch */}
-						<div className="w-16 md:w-20 mx-8"> 
-							<a href="https://www.twitch.tv/kayt_was_taken"><Image src="/icos/TwitchGlitchBlackOps.png" alt="Twitch logo" width="64" height="74"/></a>
+						{/* Youtube */}
+						<div className="index-image"> 
+							<a href="https://www.youtube.com/channel/UCb6ZkG3s5pNIl6OMoKk5Hmg"><Image src="/icos/yt_icon_mono_light.png" alt="YouTube logo" width="90" height="64"/></a>
 						</div>
 						{/* Insta */}
-						<div className="w-16 md:w-20 mx-8"> 
+						<div className="index-image"> 
 							<a href="https://www.instagram.com/kayt_was_taken"><Image src="/icos/InstagramLogoBlack.png" alt="Instagram logo" width="64" height="64"/></a>
 						</div>
-						{/* Linktree */}
-						<div className="w-16 md:w-20 mx-8"> 
-							<a href="https://www.linktr.ee/kayt_was_taken"><Image src="/icos/LinktreeLogoBlack.png" alt="Linktree logo" width="64" height="64"/></a>
+						{/* Twitter */}
+						<div className="index-image"> 
+							<a href="https://www.twitter.com/kayt_was_taken"><Image src="/icos/TwitterLogoBlack.png" alt="Twitter logo" width="70" height="57"/></a>
 						</div>
+						{/* Latest YT upload */}
+						{/* cry ill put it here later */}
 					</div>
 				</div>
 				<div className="">
